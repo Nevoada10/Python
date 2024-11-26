39b46fc3c4171060a258d10d7407e41e09023ff2
Uriel Neves Silva <144985253+Nevoada10@users.noreply.github.com>
2024-11-26 17:04:54 +0100

    .

---
+++ RandomTests/Calculations/conversiontoseconds.py
@@ -0,0 +1,9 @@
+# Conversão de anos para segundos
+anos = 21
+segundos_por_ano = 365.25 * 24 * 60 * 60  # Considerando anos bissextos
+segundos = anos * segundos_por_ano
+
+
+print(segundos_por_ano)
+print(anos)
+print(int(segundos))
